d0710a5a812b0a2892b87aa33fc07a353f2333c9
Luan Ferreira <ferreirasilva.luan13@gmail.com>
2021-08-25 18:26:07 -0300

    classes Traveller and Wagon passing all tests

--- models/Traveler.js
+++ models/Traveler.js
@@ -1,5 +1,27 @@
 class Traveler {
+constructor(name, isHealthy, food){
+        this.name = name;
+        this.isHealthy = isHealthy;
+        this.food = food;
+       
+    }
 
+    hunt(){
+        let newQty = this.food + 2;
+        this.food = newQty;
+        return `Now ${this.name} has ${newQty} amount of food.`;
+    }
+
+    eat(){
+        if(this.food > 0){
+            this.food -= 1;
+        }
+        else{
+            this.isHealthy = false;
+        }
+
+        return this.isHealthy
+    }
 }
 
 module.exports = Traveler;
--- models/Wagon.js
+++ models/Wagon.js
@@ -1,5 +1,52 @@
 class Wagon {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.passengers = [];
+    }
 
+    getAvailableSeatCount() {
+        const occupiedSeats = this.passengers.length;
+        const totalCapacity = this.capacity;
+        let availableSeats = totalCapacity - occupiedSeats;
+
+        return availableSeats;
+    }
+
+    join(passenger) {
+        const occupiedSeats = this.passengers.length;
+
+        if(occupiedSeats >= this.capacity){
+            return `All the seats are occupied!`;
+        }
+        this.passengers.push(passenger);
+        return passenger;
+    }
+
+    shouldQuarantine() {
+        const passengersList = this.passengers;
+        let status = false;
+        
+        passengersList.map(person => {
+            if(person.isHealthy === 'false'){
+                status = true;
+            }
+        });
+
+        return status;
+    }
+
+    totalFood() {
+        const passengersList = this.passengers;
+        let foodQtyList = [];
+        let totalFood = 0;
+
+        passengersList.forEach(person => {
+            foodQtyList.push(Number(person.food))
+        });
+    
+        totalFood = foodQtyList.reduce((acc, current) => acc + current);
+
+        return totalFood;
+    }
 }
-
 module.exports = Wagon;
